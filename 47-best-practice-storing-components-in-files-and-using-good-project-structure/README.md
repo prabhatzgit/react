d975b62f3148ddcd36f1c8233dba43d07636030b
mahan <prabhat.nist4@gmail.com>
2025-04-22 15:12:38 +0530

    sturcture the component file best practices

---
+++ 47-best-practice-storing-components-in-files-and-using-good-project-structure/README.md
@@ -0,0 +1,11 @@
+# All the components are in the same single file App.jsx
+
+# it will work. But, it is not recommended to keep all in same file.
+
+# If two components are super closely related and really work with each other, then use both of them
+
+# in a single file.
+
+# Create a components folder under src folder and create convenient component function code as in respective Component file.
+
+# Name the Component file as the same name as Component name
---
+++ 47-best-practice-storing-components-in-files-and-using-good-project-structure/src/App.jsx
@@ -0,0 +1,36 @@
+import { CORE_CONCEPTS } from "./components/data";
+import Header from "./components/Header/Header";
+import CoreConcept from "./components/CoreConcept";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts" />
+        <h2>Core Concepts</h2>
+        <ul>
+          <CoreConcept
+            title={CORE_CONCEPTS[0].title}
+            description={CORE_CONCEPTS[0].description}
+            image={CORE_CONCEPTS[0].image}
+          />
+          <CoreConcept
+            title={CORE_CONCEPTS[1].title}
+            description={CORE_CONCEPTS[1].description}
+            image={CORE_CONCEPTS[1].image}
+          />
+          <CoreConcept
+            title={CORE_CONCEPTS[2].title}
+            description={CORE_CONCEPTS[2].description}
+            image={CORE_CONCEPTS[2].image}
+          />
+          {/* Spread operator concept which is built in javascript to pull out all the key value pairs of an object */}
+          <CoreConcept {...CORE_CONCEPTS[0]} />
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
