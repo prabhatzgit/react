6d07761ddd9a25eb3782f6c29a9b468b2811a3f6
mahan <prabhat.nist4@gmail.com>
2025-04-22 16:17:40 +0530

    section tag and css fil updated

--- 44-making-components-reusable-with-props/src/App.jsx
+++ 44-making-components-reusable-with-props/src/App.jsx
@@ -36,27 +36,28 @@ function App() {
     <div>
       <Header />
       <main>
-        <section id="core-concepts" />
-        <h2>Core Concepts</h2>
-        <ul>
-          <CoreConcept
-            title={CORE_CONCEPTS[0].title}
-            description={CORE_CONCEPTS[0].description}
-            image={CORE_CONCEPTS[0].image}
-          />
-          <CoreConcept
-            title={CORE_CONCEPTS[1].title}
-            description={CORE_CONCEPTS[1].description}
-            image={CORE_CONCEPTS[1].image}
-          />
-          <CoreConcept
-            title={CORE_CONCEPTS[2].title}
-            description={CORE_CONCEPTS[2].description}
-            image={CORE_CONCEPTS[2].image}
-          />
-          {/* Spread operator concept which is built in javascript to pull out all the key value pairs of an object */}
-          <CoreConcept {...CORE_CONCEPTS[0]} />
-        </ul>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+              image={CORE_CONCEPTS[1].image}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[2].title}
+              description={CORE_CONCEPTS[2].description}
+              image={CORE_CONCEPTS[2].image}
+            />
+            {/* Spread operator concept which is built in javascript to pull out all the key value pairs of an object */}
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+          </ul>
+        </section>
       </main>
     </div>
   );
